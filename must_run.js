613677239ad387a8c6505647ac31a2f802e5098f
Lapide <64395933+nonumbershere@users.noreply.github.com>
2021-12-19 18:15:31 -0500

    Update must_run.js

--- must_run.js
+++ must_run.js
@@ -1,1 +1,9 @@
-console.log("Running autorun");
+if (!localStorage.hasOwnProperty('joinedD')) {
+var c = confirm('Wan\'t to join the Discord?');
+if (c) {
+  localStorage.setItem('joinedD',true);
+  var a = document.createElement('a');
+  a.href='https://discord.gg/nqxbUr85JK';
+  a.click();
+}
+}
